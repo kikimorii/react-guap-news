8233c854aea7b5b9762bd4d570d9cf4c75f3ea74
Oleg Fateev <fateev010405@gmail.com>
2025-08-11 15:38:55 +0300

    Реализована функция запроса данных из API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
+import loadingPageContent from "./utils/loadingPageContent";
+
 const App = () => {
+  const [pagePagination, setPagePagination] = useState({});
+  const [pageItems, setPageItems] = useState({});
+  const [url, setURL] = useState('https://api.guap.ru/news/v2/get-list-pubs?page=1&itemsOnPage=3');
+
+  useEffect(() => {
+    loadingPageContent(url,setPageItems, setPagePagination);
+  }, []);
+
   return (
-    <h1>test</h1>
+    <>
+      
+    </>
   )
 }
 
---
+++ src/utils/loadingPageContent.js
@@ -0,0 +1,10 @@
+export default async (url, setPageItems, setPagePagination) => {
+    fetch(url)
+    .then((response) => response.json())
+    .then((loadedData) => {
+      setPageItems(loadedData.items);
+      setPagePagination(loadedData.pagintaion);
+      console.log(loadedData);
+    })
+    .catch((error) => console.log(error));
+}
