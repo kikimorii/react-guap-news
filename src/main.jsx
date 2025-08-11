094c268bd41c42f9139037d384bcfec10f3c6931
Oleg Fateev <fateev010405@gmail.com>
2025-08-11 14:42:36 +0300

    Добавлен layout страницы, импортированы все стили с визитки сотрудника, а также функционал меню бургеров

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+import './layout/scss/main.scss';
+import './layout/js/desktopMenu.js';
+import './layout/js/mobileMenu.js';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
