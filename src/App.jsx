bf60203da7e3c7a4f52e6a6ebd14a210f9e3694c
Oleg Fateev <fateev010405@gmail.com>
2025-08-12 12:42:53 +0300

    Реализован loader в компоненте NewsList

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,15 @@ const App = () => {
   const [pagePagination, setPagePagination] = useState({});
   const [pageItems, setPageItems] = useState({});
   const [url, setURL] = useState('https://api.guap.ru/news/v2/get-list-pubs?page=1&itemsOnPage=3');
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    loadingPageContent(url, setPageItems, setPagePagination);
+    loadingPageContent(url, setPageItems, setPagePagination, setIsLoading);
   }, []);
 
   return (
     <>
-      <NewsList newsList={pageItems} />
+      <NewsList newsList={pageItems} isLoading={isLoading} />
     </>
   )
 };
--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -2,19 +2,22 @@ import NewsListItem from "./NewsListItem";
 import styles from './NewsList.module.scss';
 import ErrorMsg from "./ErrorMsg";
 
-const NewsList = ({ newsList }) => {
-    console.log(newsList.length);
-    if (newsList.length) {
-        return (
+const NewsList = ({ newsList, isLoading }) => {
+    return (
+        isLoading ? (
+            <div className={styles.loaderWrapper}>
+                <div className={styles.loader}></div>
+            </div>
+        ) : newsList.length ? (
             <ul className={styles.list}>
                 {Object.values(newsList).map((newsContent) => (
                     <NewsListItem key={newsContent.id} {...newsContent} />
                 ))}
             </ul>
+        ) : (
+            <ErrorMsg />
         )
-    } else {
-        return <ErrorMsg />
-    }
+    );
 };
 
 export default NewsList;
