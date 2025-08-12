1e33cd92593284c2369323d561b0f13dff0d4d11
Oleg Fateev <fateev010405@gmail.com>
2025-08-12 11:37:25 +0300

    Добавлено отлавливание неправильных запросов, возвращает пустой объект вместо ошибки

--- src/utils/loadingPageContent.js
+++ src/utils/loadingPageContent.js
@@ -2,8 +2,8 @@ export default async (url, setPageItems, setPagePagination) => {
     fetch(url)
     .then((response) => response.json())
     .then((loadedData) => {
-      setPageItems(loadedData.items);
-      setPagePagination(loadedData.pagintaion);
+      setPageItems(loadedData.items || {});
+      setPagePagination(loadedData.pagintaion || {});
     })
     .catch((error) => console.log(error));
 }
