dd6805d32d47827d83cd7f8b0768c416663b9d9c
Oleg Fateev <fateev010405@gmail.com>
2025-08-13 15:41:53 +0300

    Hotfix, ссылка была невалидная для fetch'а

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   const [pagePagination, setPagePagination] = useState({});
   const [numberOfPage, setNumberOfPage] = useState(1);
   const [pageItems, setPageItems] = useState({});
-  const url = `https://ai.guap.ru/news/v2/get-list-pubs?page=${numberOfPage}&itemsOnPage=10`;
+  const url = `https://api.guap.ru/news/v2/get-list-pubs?page=${numberOfPage}&itemsOnPage=10`;
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
