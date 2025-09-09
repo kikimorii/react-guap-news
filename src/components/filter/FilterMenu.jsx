b3b125ae08ca301e4ec2be8e418b5507cfb37c0b
Oleg Fateev <fateev010405@gmail.com>
2025-09-09 16:02:37 +0300

    Кнопка сброса календаря

--- src/components/filter/FilterMenu.jsx
+++ src/components/filter/FilterMenu.jsx
@@ -124,22 +124,6 @@ const FiltreMenu = ({ isFilterListVisiable, currentQueryParams, setCurrentQueryP
                     ) : ""}
                 </div>
                 <div className={styles.selectsWrapper}>
-                    {selectsContent.length !== 0 && (
-                        Object.entries(selectsContent).map(([key, { options, placeholder }]) => (
-                            <Select
-                                key={key}
-                                options={options}
-                                isMulti
-                                value={defaultValues(key, options)}
-                                onChange={(data) => handlerOnChange(key, data)}
-                                placeholder={placeholder}
-                                menuPlacement={isMobile ? "top" : "bottom"}
-                                menuShouldBlockScroll={true}
-                                styles={selectStyles}
-                                isSearchable={false}
-                            />
-                        ))
-                    )}
                     <div className={styles.datePickerWrapper}>
                         <DatePicker
                             className={isMobile ? "rmdp-mobile" : ""}
@@ -160,16 +144,36 @@ const FiltreMenu = ({ isFilterListVisiable, currentQueryParams, setCurrentQueryP
                             value={range}
                             onChange={handleDateChange}
                         />
-                        <button
-                            onClick={() => handleDateClean()}
-                            className={styles.datePickerReset}
-                            type="button"
-                        >
-                            <svg height="20" width="20" viewBox="0 0 20 20">
-                                <path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path>
-                            </svg>
-                        </button>
+                        {
+                            beginDate && (
+                                <button
+                                    onClick={() => handleDateClean()}
+                                    className={styles.datePickerReset}
+                                    type="button"
+                                >
+                                    <svg height="20" width="20" viewBox="0 0 20 20">
+                                        <path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path>
+                                    </svg>
+                                </button>
+                            )
+                        }
                     </div>
+                    {selectsContent.length !== 0 && (
+                        Object.entries(selectsContent).map(([key, { options, placeholder }]) => (
+                            <Select
+                                key={key}
+                                options={options}
+                                isMulti
+                                value={defaultValues(key, options)}
+                                onChange={(data) => handlerOnChange(key, data)}
+                                placeholder={placeholder}
+                                menuPlacement={isMobile ? "top" : "bottom"}
+                                menuShouldBlockScroll={true}
+                                styles={selectStyles}
+                                isSearchable={false}
+                            />
+                        ))
+                    )}
                     {isMobile && (<input
                         value={currentQueryParams.find ? decodeURIComponent(currentQueryParams.find) : ""}
                         className={styles.input}
